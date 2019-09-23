8001d2adbd1b2b57f0697f0f244533e9e83bea54
lizhu <lizhu@channelsoft.com>
2019-09-23 15:14:13 +0800

    test git

---
+++ src/main/java/com/springboot/springbootfocus/task/Constants.java
@@ -0,0 +1,26 @@
+package com.springboot.springbootfocus.task;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * <dl>
+ * <dt> Constants</dt>
+ * <dd>Description:</dd>
+ * <dd>Copyright: Copyright (C) 2018</dd>
+ * <dd>Company: 青牛（北京）技术有限公司</dd>
+ * <dd>CreateDate:2019/3/29</dd>
+ * </dl>
+ *
+ * @author lizhu
+ */
+public class Constants {
+
+    public static List<String> list;
+
+    public static void addData(){
+        list = new ArrayList<>();
+        list.add("1");
+
+    }
+}
